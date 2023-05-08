6d87751b0d782e62fdcee1c58f933579043b2851
Winston Lee <wlee@ucsb.edu>
2023-05-07 22:53:49 -0700

    trigger action

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Github Actions</title>
+        <title>Github Test Actions</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
