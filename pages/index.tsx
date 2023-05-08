2b472356f8ef38f6a7ab9b5969b328231b97e321
Winston Lee <wlee@ucsb.edu>
2023-05-07 22:43:01 -0700

    change to Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 
-import styles from '@/pages/index.module.css'
+import styles from "@/pages/index.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Test Github Actions</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -54,12 +54,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
