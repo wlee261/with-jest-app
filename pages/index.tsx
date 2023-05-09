c4ffa2383820f068bdfebbbc417e19f4262e8a54
Winston Lee <wlee@ucsb.edu>
2023-05-09 10:51:33 -0700

    updated workflow names

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h1 className={styles.title}>
+          We are automatically deploying this website with git actions
+        </h1>
 
         <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
